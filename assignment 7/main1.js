f1c5b88067595cadf09a50f6ec6121185ce05b3c
ketan-bhambure-bbi <141905265+ketan-bhambure-bbi@users.noreply.github.com>
2023-08-16 15:56:14 +0530

    Update main1.js

--- assignment 7/main1.js
+++ assignment 7/main1.js
@@ -1,4 +1,5 @@
 main1
+
 let flatjson = [
     {
         type: 'div', children: [
@@ -12,20 +13,29 @@ let flatjson = [
         ]
     }
 ]
-for (var i = 0; i < flatjson.length; i++) {
-    var div = document.createElement(flatjson[0].type);
-    div.setAttribute('class', 'note');
-    document.body.appendChild(div);
-    for (var j = 0; j < flatjson[i].children.length; j++) {
-        var h1 = document.createElement(flatjson[i].children[j].type);
-        h1.textContent = flatjson[i].children[j].text;
-        h1.setAttribute('class', 'note1')
-        div.appendChild(h1);
-        for (var k = 0; k < flatjson[i].children[j].children.length; k++) {
-            var l = document.createElement(flatjson[i].children[j].children[k].type);
-            l.setAttribute('class', 'note2');
-            l.textContent = flatjson[i].children[j].children[k].text;
-            h1.appendChild(l);
+function traverse(flatjson, root) {
+
+    flatjson.forEach(element => {
+
+        var ele = document.createElement(element.type);
+        if (element.text) {
+            ele.textContent = element.text;
         }
-    }
+
+        if (!root) {
+            document.body.appendChild(ele);
+        } 
+
+        else {
+            root.appendChild(ele);
+        }
+
+        if (element.children) {
+            traverse(element.children, ele);
+        }
+
+    });
+
 }
+traverse(flatjson)
+;
